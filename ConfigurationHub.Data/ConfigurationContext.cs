40a6c99f2e9ec3353e073cdb5c84cdf636b3db0c
talalmog8 <talalmog8@gmail.com>
2020-12-25 22:30:45 +0200

    config db context + inital migration

---
+++ ConfigurationHub.Data/ConfigurationContext.cs
@@ -0,0 +1,18 @@
+﻿using Configuration.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Configuration.Data
+{
+    public sealed class ConfigurationContext : DbContext
+    {
+        public ConfigurationContext(DbContextOptions<ConfigurationContext> options) : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+        
+        public DbSet<Config> Configs { get; set; }
+        public DbSet<ConfigAuthor> ConfigAuthors { get; set; }
+        public DbSet<ConfigContent> ConfigContents { get; set; }
+        public DbSet<Domain.System> ConfigSystems { get; set; }
+    }
+}
---
+++ ConfigurationHub.Data/Migrations/20201225180625_InitialCreate.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ConfigurationHub.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ConfigAuthors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: true),
+                    LastName = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ConfigAuthors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ConfigSystems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    MicroserviceName = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ConfigSystems", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Configs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    LastModified = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    AuthorId = table.Column<int>(type: "INTEGER", nullable: true),
+                    SystemId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Configs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Configs_ConfigAuthors_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "ConfigAuthors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Configs_ConfigSystems_SystemId",
+                        column: x => x.SystemId,
+                        principalTable: "ConfigSystems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ConfigContents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Content = table.Column<string>(type: "TEXT", nullable: true),
+                    ConfigId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ConfigContents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ConfigContents_Configs_ConfigId",
+                        column: x => x.ConfigId,
+                        principalTable: "Configs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConfigContents_ConfigId",
+                table: "ConfigContents",
+                column: "ConfigId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Configs_AuthorId",
+                table: "Configs",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Configs_SystemId",
+                table: "Configs",
+                column: "SystemId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ConfigContents");
+
+            migrationBuilder.DropTable(
+                name: "Configs");
+
+            migrationBuilder.DropTable(
+                name: "ConfigAuthors");
+
+            migrationBuilder.DropTable(
+                name: "ConfigSystems");
+        }
+    }
+}
