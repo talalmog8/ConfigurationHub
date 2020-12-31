1450a3b5150a87edff790dddcefd70d3c92e20de
talalmog8 <talalmog8@gmail.com>
2020-12-31 21:57:18 +0200

    adding 2 auto generated controllers for Config & ConfigAuthor
    Added to entity queries related data

---
+++ ConfigurationHub/Controllers/ConfigAuthorsController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Configuration.Data;
+using Configuration.Domain;
+
+namespace ConfigurationHub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigAuthorsController : ControllerBase
+    {
+        private readonly ConfigurationContext _context;
+
+        public ConfigAuthorsController(ConfigurationContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ConfigAuthors
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AuthorAndConfigs>>> GetConfigAuthors()
+        {
+            return  await _context.ConfigAuthors
+                .Select(x => new AuthorAndConfigs()
+                {
+                    ConfigAuthor = x, 
+                    ConfigIds  = x.Configs.Select(t => t.Id),
+                    ConfigCount = x.Configs.Count
+                }).ToListAsync();
+        }
+
+        // GET: api/ConfigAuthors/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ConfigAuthor>> GetConfigAuthor(int id)
+        {
+            var configAuthor = await _context.ConfigAuthors.FindAsync(id);
+
+            if (configAuthor == null)
+            {
+                return NotFound();
+            }
+
+            return configAuthor;
+        }
+
+        // PUT: api/ConfigAuthors/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutConfigAuthor(int id, ConfigAuthor configAuthor)
+        {
+            if (id != configAuthor.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(configAuthor).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ConfigAuthorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ConfigAuthors
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ConfigAuthor>> PostConfigAuthor(ConfigAuthor configAuthor)
+        {
+            _context.ConfigAuthors.Add(configAuthor);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetConfigAuthor", new { id = configAuthor.Id }, configAuthor);
+        }
+
+        // DELETE: api/ConfigAuthors/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteConfigAuthor(int id)
+        {
+            var configAuthor = await _context.ConfigAuthors.FindAsync(id);
+            if (configAuthor == null)
+            {
+                return NotFound();
+            }
+
+            _context.ConfigAuthors.Remove(configAuthor);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ConfigAuthorExists(int id)
+        {
+            return _context.ConfigAuthors.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ConfigurationHub/Controllers/ConfigsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Configuration.Data;
+using Configuration.Domain;
+
+namespace ConfigurationHub.Controllers
+{
+    /// <summary>
+    /// Auto Generated Controller For Config Model.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigsController : ControllerBase
+    {
+        private readonly ConfigurationContext _context;
+
+        public ConfigsController(ConfigurationContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Configs
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Config>>> GetConfigs()
+        {
+            return await _context.Configs
+                .Include(x => x.Author)
+                .Include(y => y.ConfigContent)
+                .Include(t => t.System).ToListAsync();
+        }
+
+        // GET: api/Configs/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Config>> GetConfig(int id)
+        {
+            var config = await _context.Configs
+                .Include(x => x.Author)
+                .Include(y => y.ConfigContent)
+                .Include(t => t.System)
+                .FirstAsync((c) => c.Id == id);
+
+            if (config == null)
+            {
+                return NotFound();
+            }
+
+            return config;
+        }
+
+        // PUT: api/Configs/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutConfig(int id, Config config)
+        {
+            if (id != config.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(config).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ConfigExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Configs
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Config>> PostConfig(Config config)
+        {
+            _context.Configs.Add(config);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetConfig", new { id = config.Id }, config);
+        }
+
+        // DELETE: api/Configs/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteConfig(int id)
+        {
+            var config = await _context.Configs.FindAsync(id);
+            if (config == null)
+            {
+                return NotFound();
+            }
+
+            _context.Configs.Remove(config);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ConfigExists(int id)
+        {
+            return _context.Configs.Any(e => e.Id == id);
+        }
+    }
+}
