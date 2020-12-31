e1874971f38ada47c53a22430873677c73d1c749
talalmog8 <talalmog8@gmail.com>
2020-12-31 21:35:42 +0200

    Helper type for reporting authors with their config ids

---
+++ ConfigurationHub.Domain/AuthorAndConfigs.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Configuration.Domain
+{
+    public class AuthorAndConfigs
+    {
+        public ConfigAuthor ConfigAuthor { get; set; }
+
+        public IEnumerable<int> ConfigIds { get; set; }
+
+        public long ConfigCount { get; set; }
+    }
+}
