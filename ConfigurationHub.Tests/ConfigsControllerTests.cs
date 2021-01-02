39ed1fb572989e5bec64c63fcebc5bef3559661a
talalmog8 <talalmog8@gmail.com>
2021-01-02 22:55:37 +0200

    Testing ConfigsController - GetItems, PostItem

---
+++ ConfigurationHub.Tests/ConfigsControllerTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.Json;
+using Configuration.Data;
+using Configuration.Domain;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace ConfigurationHub.Tests
+{
+    public class ConfigsControllerTests
+    {
+        protected DbContextOptions<ConfigurationContext> ContextOptions { get; }
+
+        protected ConfigsControllerTests(DbContextOptions<ConfigurationContext> contextOptions)
+        {
+            ContextOptions = contextOptions;
+            Seed();
+        }
+
+        private void Seed()
+        {
+            using (var context = new ConfigurationContext(ContextOptions))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                var author = new ConfigAuthor()
+                {
+                    FirstName = "Tal",
+                    LastName = "Almog"
+                };
+                
+                var content = new ConfigContent()
+                {
+                    Content = JsonSerializer.Serialize(new { Port = 200 })
+                };
+
+                var system = new Configuration.Domain.System()
+                {
+                    MicroserviceName = "ConfigurationHub"
+                };
+
+                var config = new Config()
+                {
+                    ConfigContent = content,
+                    Author = author,
+                    System = system
+
+                };
+
+                context.Add(config);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ ConfigurationHub.Tests/SqliteConfigsControllerTest.cs
@@ -0,0 +1,83 @@
+﻿using System.Linq;
+using System.Text.Json;
+using Configuration.Data;
+using Configuration.Domain;
+using ConfigurationHub.Controllers;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace ConfigurationHub.Tests
+{
+    public class SqliteConfigsControllerTest : ConfigsControllerTests
+    {
+        public SqliteConfigsControllerTest() : base(new DbContextOptionsBuilder<ConfigurationContext>()
+            .UseSqlite("Filename=Test.db")
+            .Options)
+        {
+
+        }
+
+        [Fact]
+        public void CanGetItems()
+        {
+            using (var context = new ConfigurationContext(ContextOptions))
+            {
+                var controller = new ConfigsController(context);
+
+                var items = controller.GetConfigs().Result.Value.ToList();
+                
+                Assert.Equal("Tal", items[0].Author.FirstName);
+                Assert.Equal("ConfigurationHub", items[0].System.MicroserviceName);
+                Assert.Equal(200, JObject.Parse(items[0].ConfigContent.Content).GetValue("Port"));
+                Assert.Single(items);
+            }
+        }
+
+        [Fact]
+        public void CanAddItem()
+        {
+            var author = new ConfigAuthor()
+            {
+                FirstName = "Yonatan",
+                LastName = "Cohen"
+            };
+
+            var content = new ConfigContent()
+            {
+                Content = JsonSerializer.Serialize(new { Port = 2000 })
+            };
+
+            var system = new Configuration.Domain.System()
+            {
+                MicroserviceName = "SystemName"
+            };
+
+            var config = new Config()
+            {
+                ConfigContent = content,
+                Author = author,
+                System = system
+
+            };
+
+            using (var context = new ConfigurationContext(ContextOptions))
+            {
+                var controller = new ConfigsController(context);
+
+                controller.PostConfig(config).Wait();
+            }
+
+            using (var context = new ConfigurationContext(ContextOptions))
+            {
+                var controller = new ConfigsController(context);
+
+                var item = controller.GetConfigs().Result.Value.ToList()[1];
+
+                Assert.Equal("Yonatan", item.Author.FirstName);
+                Assert.Equal("SystemName", item.System.MicroserviceName);
+                Assert.Equal(2000, JObject.Parse(item.ConfigContent.Content).GetValue("Port"));
+            }
+        }
+    }
+}   
