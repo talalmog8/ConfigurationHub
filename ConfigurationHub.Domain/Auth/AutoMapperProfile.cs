fba9fb1ae4d85c4e7531d9ee272378616fbfcdff
talalmog8 <talalmog8@gmail.com>
2021-01-08 12:20:17 +0200

    mapper profile for AutoMapper, user middleware

---
+++ ConfigurationHub.Domain/Auth/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace ConfigurationHub.Domain.Auth
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, AuthenticateDto>();
+            CreateMap<RegisterDto, User>();
+            CreateMap<UpdateDto, User>();
+        }
+    }
+}
