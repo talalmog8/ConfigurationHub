e3f0696d2d8e75c88265919f90eade2e121ce8d4
talalmog8 <talalmog8@gmail.com>
2021-01-08 14:52:40 +0200

    added cors policy and authentication middleware

--- ConfigurationHub/Startup.cs
+++ ConfigurationHub/Startup.cs
@@ -1,10 +1,23 @@
-using Configuration.Data;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
+using AutoMapper;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System;
+using Configuration.Data;
+using ConfigurationHub.Core.Auth;
+using ConfigurationHub.Data;
+using ConfigurationHub.Data.Repositories;
+using ConfigurationHub.Domain.Auth;
+using ConfigurationHub.General.HelperModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.OpenApi.Models;
 
 namespace ConfigurationHub
@@ -22,16 +35,73 @@ namespace ConfigurationHub
         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.AddCors();
             services.AddControllers();
+            services.AddAutoMapper(typeof(AutoMapperProfile));
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ConfigurationHub", Version = "v1" });
             });
             services.AddControllers();
+
             services.AddDbContext<ConfigurationContext>(opt =>
                opt.UseSqlite(Configuration.GetConnectionString("dev"))
                   .EnableSensitiveDataLogging()
                );
+
+            services.AddDbContext<UserContext>(opt =>
+                opt.UseSqlite(Configuration.GetConnectionString("dev"))
+                    .EnableSensitiveDataLogging()
+            );
+            
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+            services.AddAuthentication(x =>
+                {
+                    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(x =>
+                {
+                    x.Events = new JwtBearerEvents
+                    {
+                        OnTokenValidated = context =>
+                        {
+                            var userService = context.HttpContext.RequestServices.GetRequiredService<IUserService>();
+                            var userId = int.Parse(context.Principal.Identity.Name);
+                            var user = userService.GetById(userId);
+                            if (user == null)
+                                context.Fail("Unauthorized");
+                            return Task.CompletedTask;
+                        }
+                    };
+                    x.RequireHttpsMetadata = false;
+                    x.SaveToken = true;
+                    x.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
+
+            var policy = new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .Build();
+
+            services.AddMvc(options =>
+            {
+                options.Filters.Add(new AuthorizeFilter(policy));
+            });
+
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IJwtTokenUtils, JwtTokenUtils>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +116,12 @@ namespace ConfigurationHub
 
             app.UseRouting();
 
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
