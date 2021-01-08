e24c345bb70eb63d6aac2b2d055c889dea809472
talalmog8 <talalmog8@gmail.com>
2021-01-08 12:11:52 +0200

    utilties for creating jwt

---
+++ ConfigurationHub.Core/Auth/JwtTokenUtils.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ConfigurationHub.Core.Auth
+{
+    public interface IJwtTokenUtils
+    {
+        string CreateToken(string secret, string id);
+    }
+
+    public class JwtTokenUtils : IJwtTokenUtils
+    {
+        private readonly Encoding _encoding;
+        private readonly DateTime _expiry;
+
+        public JwtTokenUtils()
+        {
+            _encoding = Encoding.ASCII;
+            _expiry = DateTime.UtcNow.AddDays(7);
+        }
+
+        public JwtTokenUtils(Encoding encoding, DateTime expiry)
+        {
+            _encoding = encoding;
+            _expiry = expiry;
+        }
+
+        public string CreateToken(string secret, string id)
+        {
+            JwtSecurityTokenHandler tokenHandler = new();
+            SecurityTokenDescriptor tokenDescriptor = new()
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, id)
+                }),
+                Expires = _expiry,
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(_encoding.GetBytes(secret)),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            return tokenHandler.WriteToken(tokenHandler.CreateToken(tokenDescriptor));
+        }
+    }
+}
