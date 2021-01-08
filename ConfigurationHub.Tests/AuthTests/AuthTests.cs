500ee8b4fc4c3860552235afbced9d9b479534cd
talalmog8 <talalmog8@gmail.com>
2021-01-08 17:49:18 +0200

    added some tests for UserService class

---
+++ ConfigurationHub.Tests/AuthTests/AuthTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ConfigurationHub.Data;
+using ConfigurationHub.Domain.Auth;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConfigurationHub.Tests.AuthTests
+{
+    public class AuthTests
+    {
+        protected DbContextOptions<UserContext> ContextOptions { get; }
+
+        protected AuthTests(DbContextOptions<UserContext> contextOptions)
+        {
+            ContextOptions = contextOptions;
+            Seed();
+        }
+
+        private void Seed()
+        {
+            using (var context = new UserContext(ContextOptions))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                IEnumerable<User> users = Enumerable.Range(1, 20)
+                    .Select(x =>
+                    {
+
+                        var user = new User
+                        {
+                            FirstName = $"Tal_{x}",
+                            LastName = $"Almog_{x}",
+                            Username = $"TalAlmog45_{x}"
+                        };
+
+                        using (var hmac = new System.Security.Cryptography.HMACSHA512())
+                        {
+                            user.PasswordSalt = hmac.Key;
+                            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("test-password"));
+                        }
+
+                        return user;
+                    });
+
+
+                context.AddRange(users);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ ConfigurationHub.Tests/AuthTests/Sqlite/UserService_Tests.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using ConfigurationHub.Data;
+using ConfigurationHub.Data.Repositories;
+using ConfigurationHub.Domain.Auth;
+using ConfigurationHub.Tests.ConfigTests.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace ConfigurationHub.Tests.AuthTests.Sqlite
+{
+    public class UserService_Tests : AuthTests
+    {
+        public UserService_Tests() : base(new DbContextOptionsBuilder<UserContext>()
+        .UseSqlite($"Filename={nameof(UserService_Tests)}.db")
+            .Options)
+        {
+
+        }
+
+        [Fact]
+        public void CanCreateUser()
+        {
+            using (var context = new UserContext(ContextOptions))
+            {
+                var userService = new UserService(context);
+
+                var user = new User
+                {
+                    FirstName = "firstName",
+                    LastName = "lastName",
+                    Username = "username"
+                };
+
+                var actual = userService.Create(user, "test-password-1");
+
+                using (var hmac = new System.Security.Cryptography.HMACSHA512())
+                {
+                    user.PasswordSalt = hmac.Key;
+                    user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("test-password-1"));
+                }
+
+                Assert.Equal(user.PasswordHash, actual.PasswordHash);
+                Assert.Equal(user.PasswordSalt, actual.PasswordSalt);
+                Assert.Equal(user.Username, actual.Username);
+                Assert.Equal(user.FirstName, actual.FirstName);
+                Assert.Equal(user.LastName, actual.LastName);
+            }
+        }
+
+        [Fact]
+        public void UserNameUnique()
+        {
+            using (var context = new UserContext(ContextOptions))
+            {
+                var userService = new UserService(context);
+                
+                var user = new User
+                {
+                    FirstName = "Tal_1",
+                    LastName = "Almog_1",
+                    Username = "TalAlmog45_1"
+                };
+
+                Assert.Throws<ArgumentException>(() => userService.Create(user, "test-password-1000"));
+            }
+        }
+
+
+        [Fact]
+        public void CanDelete()
+        {
+            using (var context = new UserContext(ContextOptions))
+            {
+                var userService = new UserService(context);
+
+                var user = new User
+                {
+                    FirstName = "firstName",
+                    LastName = "lastName",
+                    Username = "username"
+                };
+
+                var actual = userService.Create(user, "test-password-1");
+
+                using (var hmac = new System.Security.Cryptography.HMACSHA512())
+                {
+                    user.PasswordSalt = hmac.Key;
+                    user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("test-password-1"));
+                }
+
+                userService.Delete(actual.Id);
+                Assert.Null(userService.GetById(actual.Id));
+            }
+        }
+
+        [Fact]
+        public void CanUpdate()
+        {
+            using (var context = new UserContext(ContextOptions))
+            {
+                var userService = new UserService(context);
+
+                var user = context.Users.First();
+
+                user.PasswordHash = null;
+                user.PasswordSalt = null;
+
+                userService.Update(user, "new-password");
+                
+                using (var hmac = new System.Security.Cryptography.HMACSHA512())
+                {
+                    user.PasswordSalt = hmac.Key;
+                    user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("new-password"));
+                }
+
+                Assert.Equal(userService.GetById(user.Id).PasswordSalt, user.PasswordSalt);
+                Assert.Equal(userService.GetById(user.Id).PasswordHash, user.PasswordHash);
+            }
+        }
+    }
+}
