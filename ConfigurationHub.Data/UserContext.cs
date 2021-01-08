0d627b42fea59d147c7d727377a308b9230e941f
talalmog8 <talalmog8@gmail.com>
2021-01-08 14:50:39 +0200

    added the user entity to db and wrote db context for it

---
+++ ConfigurationHub.Data/UserContext.cs
@@ -0,0 +1,27 @@
+﻿using Configuration.Data;
+using ConfigurationHub.Domain.Auth;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConfigurationHub.Data
+{
+    public sealed class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(e =>
+            {
+                e.HasKey(x => x.Id);
+                e.Property(x => x.Username).IsRequired();
+                e.HasIndex(x => x.Username);
+                e.HasAlternateKey(t => t.Username);
+            });
+        }
+    }
+}
