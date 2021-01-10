4db687da56bcd7b2fa13d6f72b05c476e0c9e987
talalmog8 <talalmog8@gmail.com>
2021-01-10 21:59:22 +0200

    authorization policy that allows requests to executed only if it affect the User
    that is requesting it

---
+++ ConfigurationHub/General/AuthorizationToUserOnly.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ConfigurationHub.General
+{
+    public class EmptyAuthRequirement : IAuthorizationRequirement
+    {
+    }
+    public class AuthorizationToUserOnly : AuthorizationHandler<EmptyAuthRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, EmptyAuthRequirement requirement)
+        {
+            var identity = context.User.Identity;
+
+            if (identity?.Name == null)
+            {
+                context.Fail();
+                return Task.CompletedTask;
+            }
+            if (!identity.Name.Equals(((DefaultHttpContext)context.Resource).HttpContext.Request.RouteValues["id"]))
+            {
+                context.Fail();
+                return Task.CompletedTask;
+            }
+            context.Succeed(requirement);
+            return Task.CompletedTask;
+        }
+    }
+}
