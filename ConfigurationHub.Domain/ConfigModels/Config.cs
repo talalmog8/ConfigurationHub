b97bdb0d31d6fdb2b4831d18615a0d1e079c9325
talalmog8 <talalmog8@gmail.com>
2021-02-02 14:07:25 +0200

    data annotations for Config class

---
+++ ConfigurationHub.Domain/ConfigModels/Config.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using ConfigurationHub.Domain.Auth;
+using ConfigurationHub.Domain.ConfigModels.Content;
+using ConfigurationHub.Domain.ConfigModels.MicroserviceModels;
+
+namespace ConfigurationHub.Domain.ConfigModels
+{
+    public class Config
+    {
+        public Config()
+        {
+            LastModified = DateTime.UtcNow;
+        }
+
+        [Required] public int Id { get; set; }
+        public DateTime LastModified { get; set; }
+        [Required] public User Author { get; set; }
+        [Required] public ConfigContent ConfigContent { get; set; }
+        [Required] public Microservice Microservice { get; set; }
+    }
+}
