bfb17ddae415bf5c6201d748edf7547825d81b6d
talalmog8 <talalmog8@gmail.com>
2021-02-06 19:02:22 +0200

    assembly scanning code

---
+++ ConfigurationHub.Core/DI/AssemblyScanningExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using ConfigurationHub.General;
+using Microsoft.Extensions.DependencyInjection;
+using Scrutor;
+
+namespace ConfigurationHub.Core.DI
+{
+    public static class AssemblyScanningExtensions
+    {
+        public static IImplementationTypeSelector InjectableAttributes(this IImplementationTypeSelector selector)
+        {
+            Enum.GetValues(typeof(ServiceLifetime))
+                .Cast<ServiceLifetime>()
+                .ForEach(item => selector = selector.InjectableAttribute(item));
+
+            return selector;
+        }
+
+        public static IImplementationTypeSelector InjectableAttribute(this IImplementationTypeSelector selector, ServiceLifetime lifeTime)
+        {
+            return selector.AddClasses(c => c.WithAttribute<InjectableAttribute>(i => i.Lifetime == lifeTime))
+                .AsImplementedInterfaces()
+                .WithLifetime(lifeTime);
+        }
+    }
+}
